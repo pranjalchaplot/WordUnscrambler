c03ddd0d2d6c2cbf19715dd0e6f08da39fb10fbc
Prateek Chaplot <prateekchaplot96@gmail.com>
2018-11-11 00:45:21 +0530

    WordMatcher class completed

--- WordUnscrambler/Workers/WordMatcher.cs
+++ WordUnscrambler/Workers/WordMatcher.cs
@@ -6,8 +6,49 @@ namespace WordUnscrambler.Workers
 {
     class WordMatcher
     {
-        internal List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
         {
+            var matchedWords = new List<MatchedWord>();
+
+            foreach (var scrambledWord in scrambledWords)
+            {
+                foreach (var word in wordList)
+                {
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                    }
+                    else
+                    {
+                        var scrambledWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambledWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord = new string(scrambledWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if (sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                        }
+                    }
+                }
+            }
+
+            return matchedWords;
+        }
+
+        private MatchedWord BuildMatchedWord(string scrambleWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWord = scrambleWord,
+                Word = word
+            };
+
+            return matchedWord;
         }
     }
 }
