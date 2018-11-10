2cca9b03e0629c75ee1caf9cd3511929978318f0
Prateek Chaplot <prateekchaplot96@gmail.com>
2018-11-10 19:47:01 +0530

    Code skeleton completed

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -10,12 +10,50 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            bool continueWordUnscramble = true;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            do
+            {
+                Console.WriteLine("Enter the option - F for File and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "M":
+                        Console.WriteLine("Enter scrambled words file name: ");
+                        ExecuteScrambledWordsFileScenario();
+                        break;
+                    case "F":
+                        Console.WriteLine("Enter scrambled words manually: ");
+                        ExecuteScrambledWordsManually();
+                        break;
+                    default:
+                        Console.WriteLine("Option was not recognized.");
+                        break;
+                }
+
+                var continueWordUnscrambleDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueWordUnscrambleDecision = Console.ReadLine() ?? string.Empty;
+                } while (
+                    !continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                    !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+                continueWordUnscramble = continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+            } while (continueWordUnscramble);
+        }
+
+        private static void ExecuteScrambledWordsManually()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void ExecuteScrambledWordsFileScenario()
+        {
+            //throw new NotImplementedException();
         }
     }
 }
