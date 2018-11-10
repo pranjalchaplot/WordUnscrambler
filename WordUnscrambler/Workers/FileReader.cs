ef4c4a31d4971acc3ab998a7ce116be307f8f65f
Prateek Chaplot <prateekchaplot96@gmail.com>
2018-11-10 23:50:59 +0530

    FileReader class completed

--- WordUnscrambler/Workers/FileReader.cs
+++ WordUnscrambler/Workers/FileReader.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.IO;
 
 namespace WordUnscrambler.Workers
 {
     class FileReader
     {
-        internal string[] Read(string filename)
+        public string[] Read(string filename)
         {
-            throw new NotImplementedException();
+            string[] fileContent;
+            try
+            {
+                fileContent = File.ReadAllLines(filename);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+            return fileContent;
         }
     }
 }
